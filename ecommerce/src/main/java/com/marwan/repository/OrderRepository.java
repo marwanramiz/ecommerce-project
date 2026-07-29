f6c38f6e98a2cb73b584880815c76d956146717c
Marwan <727724euit136@skcet.ac.in>
2026-07-29 23:38:44 +0530

    Added OrderService and renamed OrderRepository

--- ecommerce/src/main/java/com/marwan/repository/OrderRepository.java
+++ ecommerce/src/main/java/com/marwan/repository/OrderRepository.java
@@ -2,5 +2,5 @@ package com.marwan.repository;
 import com.marwan.model.Order;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface OrderService extends MongoRepository<Order,String>{
+public interface OrderRepository extends MongoRepository<Order,String>{
 }
